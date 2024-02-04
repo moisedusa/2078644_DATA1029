98c88de3edbf5c86b912b90ea6b3bba48134c62f
Moise Muneza <moisedusaidie@gmail.com>
2024-02-04 14:48:06 -0400

    solution du devoir3 SQL

---
+++ Moise_devoir3SQL.sql
@@ -0,0 +1,47 @@
+-- 1. La liste des paires (auteur, éditeur) demeurant dans la même ville
+SELECT au_fname as auteur  , pub_name as editeur
+FROM   authors , publishers
+WHERE authors.city = publishers.city;
+
+-- 2. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant
+-- aussi les auteurs qui ne répondent pas à ce critère.
+SELECT au_fname as auteur  , pub_name as editeur
+FROM   authors  left join publishers
+on  authors.city = publishers.city;
+
+-- 3. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant
+-- aussi les editeurs qui ne répondent pas à ce critère.
+SELECT au_fname as auteur  , pub_name as editeur
+FROM   authors  right join publishers
+on  authors.city = publishers.city;
+
+-- 4. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant les
+-- auteurs et les éditeurs qui ne répondent pas à ce critere.
+SELECT au_fname as auteur  , pub_name as editeur
+FROM   authors  left join publishers
+on  authors.city = publishers.city
+UNION 
+SELECT au_fname as auteur  , pub_name as editeur
+FROM   authors  right join publishers
+on  authors.city = publishers.city;
+
+-- 5.Effectif(nombre) d'employes par niveau d'experience
+select COUNT(emp_id) as nombre ,job_lvl as niveau_experience 
+from employees
+group by job_lvl;
+
+-- 6.Liste des employes par maison d'edition
+SELECT   e.fname, p.pub_id
+FROM employees e , publishers p 
+where e.pub_id = p.pub_id;
+
+-- 7. Salaires horaires moyens des employes par maison d'edition
+SELECT MIN(salary),  e.fname, p.pub_id
+FROM employees e , publishers p 
+where e.pub_id = p.pub_id group by e.pub_id ;
+
+-- 8. Effectif(nombre) d'employées de niveau SEINIOR par maison d'edition
+select count(*) as nombre_employe , e.job_lvl as niveau
+FROM employees e join publishers p 
+on e.pub_id = p.pub_id where e.job_lvl="SEINIOR" ;
+
